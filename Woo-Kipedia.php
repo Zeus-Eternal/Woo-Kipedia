d88501a80bd90808d190edccdbdfbe754bf0485c
Zeus-Eternal <121900625+Zeus-Eternal@users.noreply.github.com>
2023-09-18 03:56:49 -0400

    Update Woo-Kipedia.php

--- Woo-Kipedia.php
+++ Woo-Kipedia.php
@@ -1,8 +1,8 @@
 <?php
 /*
 Plugin Name: Woo-Kipedia
-Description: Display WooCommerce Product pages as Wiki's with Shortcode
-Version: 0.2.5
+Description: Display WooCommerce Product pages as Wikis with Shortcodes
+Version: 0.1.6
 Author: Zeus The Eternal
 */
 
@@ -33,7 +33,8 @@ function woo_kipedia_register_settings_fields() {
     register_setting('woo-kipedia-settings-group', 'woo-kipedia-read-more-text', 'sanitize_text_field');
     register_setting('woo-kipedia-settings-group', 'woo-kipedia-button-text', 'sanitize_text_field');
     register_setting('woo-kipedia-settings-group', 'woo-kipedia-button-font-size', 'woo_kipedia_sanitize_font_size');
-    register_setting('woo-kipedia-settings-group', 'woo-kipedia-link-as-button', 'sanitize_text_field'); // New option
+    register_setting('woo-kipedia-settings-group', 'woo-kipedia-link-as-button', 'sanitize_text_field');
+    register_setting('woo-kipedia-settings-group', 'woo-kipedia-wooki-page', 'absint');
 }
 
 // Sanitization callback functions
@@ -64,7 +65,7 @@ function woo_kipedia_settings_page() {
             <a href="?page=woo-kipedia-settings&tab=usage"
                class="nav-tab <?php echo isset($_GET['tab']) && $_GET['tab'] === 'usage' ? 'nav-tab-active' : ''; ?>">Usage Instructions</a>
             <a href="?page=woo-kipedia-settings&tab=info"
-               class="nav-tab <?php echo isset($_GET['tab']) && $_GET['tab'] === 'info' ? 'nav-tab-active' : ''; ?>">Info</a>
+               class="nav-tab <?php echo isset($_GET['tab']) && $_GET['tab'] === 'info' ? 'nav-tab-active' : ''; ?>">Plugin Info</a>
         </h2>
         <form method="post" action="options.php">
             <?php settings_fields('woo-kipedia-settings-group'); ?>
@@ -80,114 +81,97 @@ function woo_kipedia_settings_page() {
                         <th scope="row">Order By</th>
                         <td>
                             <select name="woo-kipedia-order-by">
-                                <option value="date" <?php selected(get_option('woo-kipedia-order-by', 'date'), 'date'); ?>>
-                                    Date
-                                </option>
-                                <option value="title" <?php selected(get_option('woo-kipedia-order-by', 'date'), 'title'); ?>>
-                                    Title
-                                </option>
-                                <option value="rand" <?php selected(get_option('woo-kipedia-order-by', 'date'), 'rand'); ?>>
-                                    Random
-                                </option>
+                                <option value="date" <?php selected(get_option('woo-kipedia-order-by'), 'date'); ?>>Date</option>
+                                <option value="title" <?php selected(get_option('woo-kipedia-order-by'), 'title'); ?>>Title</option>
+                                <option value="price" <?php selected(get_option('woo-kipedia-order-by'), 'price'); ?>>Price</option>
                             </select>
                         </td>
                     </tr>
                     <tr valign="top">
-                        <th scope="row">Sort Order</th>
+                        <th scope="row">Order</th>
                         <td>
                             <select name="woo-kipedia-order">
-                                <option value="asc" <?php selected(get_option('woo-kipedia-order', 'asc'), 'asc'); ?>>
-                                    Ascending
-                                </option>
-                                <option value="desc" <?php selected(get_option('woo-kipedia-order', 'asc'), 'desc'); ?>>
-                                    Descending
-                                </option>
+                                <option value="asc" <?php selected(get_option('woo-kipedia-order'), 'asc'); ?>>Ascending</option>
+                                <option value="desc" <?php selected(get_option('woo-kipedia-order'), 'desc'); ?>>Descending</option>
                             </select>
                         </td>
                     </tr>
                     <tr valign="top">
-                        <th scope="row">Max Short Description Words</th>
+                        <th scope="row">Max Description Words</th>
                         <td><input type="number" name="woo-kipedia-max-description-words"
-                                   value="<?php echo esc_attr(get_option('woo-kipedia-max-description-words', 500)); ?>"/>
-                        </td>
+                                   value="<?php echo esc_attr(get_option('woo-kipedia-max-description-words', 500)); ?>"/></td>
                     </tr>
                     <tr valign="top">
-                        <th scope="row">Show/Hide Prices</th>
+                        <th scope="row">Show Price</th>
                         <td>
-                            <label for="woo-kipedia-show-price">
-                                <input type="checkbox" id="woo-kipedia-show-price" name="woo-kipedia-show-price"
-                                       value="yes" <?php checked(get_option('woo-kipedia-show-price', 'yes'), 'yes'); ?> />
-                                Show Prices
-                            </label>
+                            <label><input type="radio" name="woo-kipedia-show-price" value="yes"
+                                          <?php checked(get_option('woo-kipedia-show-price'), 'yes'); ?>>Yes</label>
+                            <label><input type="radio" name="woo-kipedia-show-price" value="no"
+                                          <?php checked(get_option('woo-kipedia-show-price'), 'no'); ?>>No</label>
                         </td>
                     </tr>
                     <tr valign="top">
-                        <th scope="row">Show/Hide Thumbnail Images</th>
+                        <th scope="row">Show Thumbnail</th>
                         <td>
-                            <label for="woo-kipedia-show-thumbnail">
-                                <input type="checkbox" id="woo-kipedia-show-thumbnail"
-                                       name="woo-kipedia-show-thumbnail" value="yes"
-                                    <?php checked(get_option('woo-kipedia-show-thumbnail', 'yes'), 'yes'); ?> />
-                                Show Thumbnail Images
-                            </label>
+                            <label><input type="radio" name="woo-kipedia-show-thumbnail" value="yes"
+                                          <?php checked(get_option('woo-kipedia-show-thumbnail'), 'yes'); ?>>Yes</label>
+                            <label><input type="radio" name="woo-kipedia-show-thumbnail" value="no"
+                                          <?php checked(get_option('woo-kipedia-show-thumbnail'), 'no'); ?>>No</label>
                         </td>
                     </tr>
                     <tr valign="top">
-                        <th scope="row">Show Read More Button</th>
+                        <th scope="row">Show Read More</th>
                         <td>
-                            <label for="woo-kipedia-show-read-more">
-                                <input type="checkbox" id="woo-kipedia-show-read-more"
-                                       name="woo-kipedia-show-read-more" value="yes"
-                                    <?php checked(get_option('woo-kipedia-show-read-more', 'yes'), 'yes'); ?> />
-                                Show Read More
-                            </label>
+                            <label><input type="radio" name="woo-kipedia-show-read-more" value="yes"
+                                          <?php checked(get_option('woo-kipedia-show-read-more'), 'yes'); ?>>Yes</label>
+                            <label><input type="radio" name="woo-kipedia-show-read-more" value="no"
+                                          <?php checked(get_option('woo-kipedia-show-read-more'), 'no'); ?>>No</label>
                         </td>
                     </tr>
                     <tr valign="top">
-                        <th scope="row">Read More Button Text</th>
+                        <th scope="row">Read More Text</th>
                         <td><input type="text" name="woo-kipedia-read-more-text"
-                                   value="<?php echo esc_attr(get_option('woo-kipedia-read-more-text', 'Read Woo-Ki')); ?>"/>
-                        </td>
+                                   value="<?php echo esc_attr(get_option('woo-kipedia-read-more-text', 'Read Woo-Ki')); ?>"/></td>
                     </tr>
                     <tr valign="top">
                         <th scope="row">Link as Button</th>
                         <td>
-                            <label for="woo-kipedia-link-as-button">
-                                <input type="checkbox" id="woo-kipedia-link-as-button"
-                                       name="woo-kipedia-link-as-button" value="yes"
-                                    <?php checked(get_option('woo-kipedia-link-as-button', 'no'), 'yes'); ?> />
-                                Link as Button
-                            </label>
+                            <label><input type="radio" name="woo-kipedia-link-as-button" value="yes"
+                                          <?php checked(get_option('woo-kipedia-link-as-button'), 'yes'); ?>>Yes</label>
+                            <label><input type="radio" name="woo-kipedia-link-as-button" value="no"
+                                          <?php checked(get_option('woo-kipedia-link-as-button'), 'no'); ?>>No</label>
+                        </td>
+                    </tr>
+                    <tr valign="top">
+                        <th scope="row">Select Wooki Page</th>
+                        <td>
+                            <select name="woo-kipedia-wooki-page">
+                                <option value="">Select a page</option>
+                                <?php
+                                $pages = get_pages();
+                                foreach ($pages as $page) {
+                                    $selected = selected(get_option('woo-kipedia-wooki-page'), $page->ID, false);
+                                    echo "<option value='{$page->ID}' {$selected}>{$page->post_title}</option>";
+                                }
+                                ?>
+                            </select>
                         </td>
                     </tr>
                 </table>
+                <!-- Add a button to save settings -->
+                <tr valign="top">
+                    <th scope="row"></th>
+                    <td>
+                        <?php wp_nonce_field('woo-kipedia-nonce', 'woo-kipedia-security'); ?>
+                        <button type="button" id="woo-kipedia-save-settings" class="button button-primary">Save Settings</button>
+                    </td>
+                </tr>
             <?php } ?>
-            <?php if (isset($_GET['tab']) && $_GET['tab'] === 'info') { ?>
-                <div class="woo-kipedia-info">
-                    <h3>About Woo-Kipedia Plugin</h3>
-                    <p>
-                        Woo-Kipedia is a WordPress plugin that allows you to display WooCommerce products and related information using shortcodes. It provides various shortcodes to help you showcase products and their details on your website.
-                    </p>
-                    <p>
-                        <strong>Plugin Name:</strong> Woo-Kipedia<br>
-                        <strong>Version:</strong> 1.0<br>
-                        <strong>Author:</strong> Your Name
-                    </p>
-                    <p>
-                        For more information and support, visit the <a href="https://example.com/woo-kipedia-plugin"
-                                                                      target="_blank">Woo-Kipedia Plugin Page</a>.
-                    </p>
-                </div>
-            <?php } ?>
-            <?php if (isset($_GET['tab']) && $_GET['tab'] === 'usage') { ?>
-                <div class="woo-kipedia-usage-instructions">
-                    <?php echo woo_kipedia_shortcode_usage_instructions(); ?>
-                    <h3>Shortcodes for Displaying Product Information</h3>
-                    <p>Use the following shortcodes to display various WooCommerce product information:</p>
-                    <!-- Shortcode descriptions here -->
-                </div>
-            <?php } ?>
-            <?php submit_button(); ?>
+            <?php if (isset($_GET['tab']) && $_GET['tab'] === 'usage') {
+                echo woo_kipedia_shortcode_usage_instructions();
+            } elseif (isset($_GET['tab']) && $_GET['tab'] === 'info') {
+                echo woo_kipedia_plugin_info();
+            } ?>
         </form>
     </div>
     <?php
@@ -205,7 +189,15 @@ function woo_kipedia_shortcode($atts) {
         'show_read_more' => get_option('woo-kipedia-show-read-more', 'yes'),
         'read_more_text' => get_option('woo-kipedia-read-more-text', 'Read Woo-Ki'),
         'link_as_button' => get_option('woo-kipedia-link-as-button', 'no'),
+        'wooki_page' => get_option('woo-kipedia-wooki-page', ''), // New attribute
     ), $atts);
+
+    // Retrieve the content of the selected Wooki page
+    if (!empty($atts['wooki_page'])) {
+        $wooki_content = get_post_field('post_content', $atts['wooki_page']);
+    } else {
+        $wooki_content = 'No Wooki page selected.';
+    }
 
     // Query WooCommerce products
     $args = array(
@@ -220,6 +212,10 @@ function woo_kipedia_shortcode($atts) {
     ob_start();
 
     if ($products->have_posts()) {
+        // Display the Wooki page content
+        echo '<div class="wooki-content">' . $wooki_content . '</div>';
+
+        // Add code here to display WooCommerce product information
         while ($products->have_posts()) {
             $products->the_post();
             global $product;
@@ -267,7 +263,6 @@ function woo_kipedia_shortcode($atts) {
 
             <?php
         }
-
         // Add pagination
         echo '<div class="woo-kipedia-pagination">';
         echo paginate_links(array(
@@ -294,16 +289,29 @@ function woo_kipedia_shortcode_usage_instructions() {
     <pre>[woo-kipedia]</pre>
     <p>This shortcode supports the following attributes:</p>
     <ul>
-        <li><code>per_page</code> (default: 20) - Number of products to display per page.</li>
-        <li><code>order_by</code> (default: 'date') - Order products by 'date', 'title', or 'rand' (random).</li>
-        <li><code>order</code> (default: 'asc') - Sort products in 'asc' (ascending) or 'desc' (descending) order.</li>
-        <li><code>max_description_words</code> (default: 500) - Maximum number of words to display in the short description.</li>
-        <li><code>show_price</code> (default: 'yes') - Show product prices ('yes' or 'no').</li>
-        <li><code>show_thumbnail</code> (default: 'yes') - Show product thumbnail images ('yes' or 'no').</li>
-        <li><code>show_read_more</code> (default: 'yes') - Show the "Read More" link or button ('yes' or 'no').</li>
-        <li><code>read_more_text</code> (default: 'Read Woo-Ki') - Text for the "Read More" link or button.</li>
-        <li><code>link_as_button</code> (default: 'no') - Display the "Read More" link as a button ('yes' or 'no').</li>
+        <li><code>per_page</code> - Number of products to display per page.</li>
+        <li><code>order_by</code> - Order products by 'date', 'title', or 'price'.</li>
+        <li><code>order</code> - Order products in 'asc' (ascending) or 'desc' (descending) order.</li>
+        <li><code>max_description_words</code> - Maximum number of words to display in the product description.</li>
+        <li><code>show_price</code> - Whether to show the product price ('yes' or 'no').</li>
+        <li><code>show_thumbnail</code> - Whether to show the product thumbnail ('yes' or 'no').</li>
+        <li><code>show_read_more</code> - Whether to show the 'Read More' link ('yes' or 'no').</li>
+        <li><code>read_more_text</code> - Text for the 'Read More' link.</li>
+        <li><code>link_as_button</code> - Whether to display the 'Read More' link as a button ('yes' or 'no').</li>
+        <li><code>wooki_page</code> - Select a page to display as Wooki content.</li>
     </ul>
+    <?php
+    return ob_get_clean();
+}
+
+// Plugin info
+function woo_kipedia_plugin_info() {
+    ob_start();
+    ?>
+    <h2>Woo-Kipedia Plugin Information</h2>
+    <p>Author: Zeus The Eternal</p>
+    <p>Version: 0.1.6</p>
+    <p>Description: Display WooCommerce Product pages as Wikis with Shortcodes</p>
     <?php
     return ob_get_clean();
 }
@@ -314,3 +322,35 @@ function woo_kipedia_enqueue_styles() {
 }
 
 add_action('wp_enqueue_scripts', 'woo_kipedia_enqueue_styles');
+
+// Enqueue the JavaScript file
+function woo_kipedia_enqueue_admin_scripts() {
+    wp_enqueue_script('woo-kipedia-admin', plugin_dir_url(__FILE__) . 'js/woo-kipedia-admin.js', array('jquery'), '1.0', true);
+
+    // Pass Ajax URL to script.js
+    wp_localize_script('woo-kipedia-admin', 'wooKipediaAjax', array(
+        'ajax_url' => admin_url('admin-ajax.php'),
+        'security' => wp_create_nonce('woo-kipedia-nonce'),
+    ));
+}
+
+add_action('admin_enqueue_scripts', 'woo_kipedia_enqueue_admin_scripts');
+
+// Ajax handler to save settings
+function woo_kipedia_save_settings() {
+    // Check nonce for security
+    check_ajax_referer('woo-kipedia-nonce', 'security');
+
+    // Process and save settings here
+    $per_page = sanitize_text_field($_POST['per_page']);
+    // Save other settings here
+
+    // Example: Update per_page option
+    update_option('woo-kipedia-per-page', $per_page);
+
+    // Return a success response
+    wp_send_json_success();
+}
+
+add_action('wp_ajax_woo_kipedia_save_settings', 'woo_kipedia_save_settings');
+add_action('wp_ajax_nopriv_woo_kipedia_save_settings', 'woo_kipedia_save_settings');
